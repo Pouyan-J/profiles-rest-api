0e5b42391cc3e6645b9ef43864a7441924eb0c89
Pouyan <pouyan.j@protonmail.com>
2021-09-27 21:52:07 +0330

    Fixed bugs

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -3,10 +3,8 @@ from rest_framework import permissions
 class UpdateOwnProfile(permissions.BasePermission):
     """Allow users to edit their own profile"""
 
-    def has_object_permissions(self, request, veiw, obj):
+    def has_object_permission(self, request, view, obj):
         """Check user is trying to edit their own profile"""
-
         if request.method in permissions.SAFE_METHODS:
             return True
-
-        return object.id == request.user.id
+        return obj.id == request.user.id
